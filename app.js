b84aa8386726cc2ba8374898714e07be1e901b5c
pro-so <se.amin.2025@gmail.com>
2025-10-16 01:52:50 +0300

    Remove duplicate static file serving for the public directory

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static('public'));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/", cartRouter);
